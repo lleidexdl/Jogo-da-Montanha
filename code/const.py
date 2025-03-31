cd673c5a491fbf6b158fc5082547c68e74547f29
Leide <leidehelensgf@gmail.com>
2025-03-30 21:31:57 -0400

    creating asset folder Inserting all assets Creating the menu:background, sound and fonts Creating the cont file

---
+++ code/const.py
@@ -0,0 +1,17 @@
+from pygame.examples.grid import WINDOW_WIDTH
+
+#C
+COLOR_ORANGE = (255,128,0)
+COLOR_WHITE = (255,255,255)
+
+#M
+MENU_OPTION = ('NEW GAME 1P,'
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+
+#W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/game.py
+++ code/game.py
@@ -2,16 +2,14 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
-
-
-
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
         while True:
@@ -19,10 +17,6 @@ class Game:
             menu.run()
             pass
 
-          #   for event in pygame.event.get():
-          #      if event.type == pygame.QUIT:
-          #       pygame.quit() #Close Window
-           #      quit() #End Window
 
 
 
